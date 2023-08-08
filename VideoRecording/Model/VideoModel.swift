f17910bba7c38c1f8860d752c92824cfd37b10d8
Omoyeni <babatundeabike3@gmail.com>
2023-08-08 17:00:57 +0100

    Create VideoModel.swift

---
+++ VideoRecording/Model/VideoModel.swift
@@ -0,0 +1,16 @@
+//
+//  VideoModel.swift
+//  VideoRecording
+//
+//  Created by Sharon Omoyeni Babatunde on 08/08/2023.
+//
+
+import Foundation
+
+struct VideoModel: Codable {
+    
+    var videoLink: String?
+    var videoDuration: String?
+    var tag: String?
+//    var isEditing: Bool?
+}
